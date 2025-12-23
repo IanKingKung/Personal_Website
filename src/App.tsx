bc86704df497bd5d34ab9bf6ea1231420ec54219
Ian Kung <iankung.az@gmail.com>
2025-12-22 19:03:52 -0700

    Fix TypeScript Issue to Enable importing tsx files.

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ function App() {
   return (
     <>
       <div>
+        <text>Hi This is my personal website</text>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
