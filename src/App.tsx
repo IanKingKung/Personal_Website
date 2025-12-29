b969e13d9e351ebb259c2d97f291784b3162a1b7
Ian Kung <iankung.az@gmail.com>
2025-12-28 17:38:40 -0700

    Set Up Basic Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Navbar from './Navbar'
 import './App.css'
 
 function App() {
@@ -9,26 +10,10 @@ function App() {
   return (
     <>
       <div>
-        <text>Hi This is my personal website</text>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <text>Hi this is my personal website</text>
+        <h3>Currently Under Construction</h3>
+        <Navbar />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
---
+++ src/navbar.tsx
@@ -0,0 +1,15 @@
+import './navbar.css'
+
+function Navbar() {
+
+
+    return (
+        <>
+            <div className='container'>
+                <h1 style={{ color: 'white' }}>My Navbar</h1>
+            </div>
+        </>
+    )
+}
+
+export default Navbar;
