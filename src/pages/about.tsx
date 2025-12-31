3635353605b8e565f7e00faca20c582e5982b305
Ian Kung <iankung.az@gmail.com>
2025-12-31 01:23:09 -0700

    Add Tailwind and Some CSS Components from ShadCn

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,7 +1,13 @@
+import '../pages_css/about.css'
+import { Meteors } from "@/components/ui/meteors";
+
 function About() {
     return (
         <>
-            <text>home page</text>
+            <div className="relative h-[500px] w-full overflow-hidden">
+                <Meteors number={400}/>
+            </div>
+            <text className="my-card">Ian Kung</text>
         </>
     )
 }
